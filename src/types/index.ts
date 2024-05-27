aaa713b67ccadc883b369b48b88dc434d82adfbb
Mary Khritanko <madec.1st.87@gmail.com>
2024-05-27 18:20:39 +0300

    описана документация и архитектура

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,20 +22,6 @@ interface ICardData {
   price: number|null;
 }
 
-interface IPaymentForm {
-  payment: string;
-  address: string;
-
-  submit(): void;
-}
-
-interface IContactsForm {
-  email: string;
-  phone: number;
-  
-  submit(): void;
-}
-
 interface IOrder {
   items: Pick<ICardData, 'id' | 'title' | 'price'>[];
   totalPrice: number;
@@ -47,9 +33,59 @@ interface IBasketOperations {
   placeOrder(): void;
 }
 
+interface IPaymentForm {
+  payment: string;
+  address: string;
+
+  selectPaymentMethod(method: 'online' | 'onDelivery'): void;
+  enterAddress(inputAddress: string): void;
+  submit(): void;
+}
+
+interface IContactsForm {
+  email: string;
+  phone: number;
+  
+  enterEmail(inputEmail: string): void;
+  enterPhone(inputPhone: number): void;
+  submit(): void;
+}
+
 type TCardsArray = ICardData[];
 
 type TClient = IPaymentForm & IContactsForm;
+
+type TTotalPrice = Pick<IOrder, 'totalPrice'>;
+
+class CardApi implements IApi {
+  request<T>(request: IRequest<T>): Promise<IResponse<T>> {
+    return fetch(request.url, {
+      method: 'GET',
+      body: request.data ? JSON.stringify(request.data) : null,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => response.json()
+    .then(data => ({ status: response.status, data })))
+    .catch(() => ({ status: 500, data: null }));
+  }
+}
+
+class ClientApi implements IApi {
+  request<T>(request: IRequest<T>): Promise<IResponse<T>> {
+    return fetch(request.url, {
+      method: 'POST',
+      body: request.data ? JSON.stringify(request.data) : null,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => response.json()
+    .then(data => ({ status: response.status, data })))
+    .catch(() => ({ status: 500, data: null }));
+  }
+}
 
 class Card implements ICardData{
   image: string;
@@ -127,5 +163,65 @@ class Basket implements IOrder, IBasketOperations {
     console.log(`Ваш заказ успешно оформлен: ${order}`);
     this.items = [];
     this.totalPrice = 0;
+  }
+}
+
+class PaymentPopup implements IPaymentForm {
+  payment: string;
+  address: string;
+
+  constructor() {
+    this.payment = '';
+    this.address = '';
+  }
+
+  selectPaymentMethod(method: 'online' | 'onDelivery') {
+    this.payment = method;
+  }
+
+  enterAddress(inputAddress: string) {
+    this.address = inputAddress;
+  }
+
+  submit() {
+    if (!this.payment || !this.address) {
+      console.error('Необходимо выбрать метод оплаты и ввести адрес.');
+      return;
+    }
+    console.log(`Метод оплаты: ${this.payment}, Адрес: ${this.address}`);
+  }
+}
+
+class ContactsPopup implements IContactsForm {
+  email: string;
+  phone: number;
+
+  constructor(email: string, phone: number) {
+    this.email = email;
+    this.phone = phone;
+  }
+
+  enterEmail(inputEmail: string) {
+    this.email = inputEmail;
+  }
+
+  enterPhone(inputPhone: number) {
+    this.phone = inputPhone;
+  }
+
+  submit() {
+    if (!this.email || !this.phone) {
+      console.error('Необходимо ввести адрес электронной почты и номер телефона.');
+      return;
+    }
+    console.log(`Email: ${this.email}, телефон: ${this.phone}`);
+  }
+}
+
+class SuccessfulOrder implements TTotalPrice {
+  totalPrice: number;
+
+  constructor(totalPrice: number) {
+    this.totalPrice = totalPrice;
   }
 }
